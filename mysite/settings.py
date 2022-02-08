8485f97aa65452b2a421e54d09c16e52ef5eeef4
Muhammad Jamil <mjamil13@gmail.com>
2022-02-08 12:12:27 +0500

    edit

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'selfag.herokuapp.com',
+    '127.0.0.1',
 ]
 
 
--- pms/dt.py
+++ pms/dt.py
@@ -1,4 +1,8 @@
 from datetime import date
 a=date.today()
 y=a.month
+y=y+3
+b=date(2000,5,15)
+c=a-b
+print(c)
 print("year",y)
