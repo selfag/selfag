c330d21d82e28a547b5a1520ab8939f6e439848e
Muhammad Jamil <mjamil13@gmail.com>
2022-02-14 13:10:45 +0500

    edit

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,21 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
 # Create your views here.
+def login(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user=auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+           
+        else:
+            messages.info(request, "creditial are invalid")
+            return render(request, 'login.html')
+    return render(request, 'login.html')
+
 def register(request):
     if request.method=="POST":
         first_name=request.POST['fname']
@@ -15,15 +30,17 @@ def register(request):
         if password1==password2:
             if User.objects.filter(username=username).exists():
                 messages.info(request, "Username already Taken")
+                return render(request, 'register.html')
             elif User.objects.filter(email=email).exists():
                 messages.info(request, "This email already registered")
+                return render(request, 'register.html')
             user=User.objects.create_user(username=username,first_name=first_name, last_name=last_name, password=password1,email=email)
             user.save()
-            return redirect("/")
+            return redirect("login.html")
         else:
-            print("password in not matching")
+            messages.info(request, "Password not matching")
             return render(request, 'register.html')
-    return render(request, 'register.html')
+    return render(request, 'login.html')
 
 
     
